bac8be67bce3330235806e23493cb4aa48d302f4
Mao Kurihara <mao.kurihara@gmail.com>
2022-09-13 15:11:07 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+
+
+const fastify = require('fastify')({
+	logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+	reply.header("set-cookie", "mytoken=12345;HttpOnly")
+	reply.send({ hello: 'world' })
+})
+
+// ユーザーが保存していたものと一致するかしない
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+	if (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+	// Server is now listening on ${address}
+})
